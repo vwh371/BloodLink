2954295f07597de7db0dbad28aea6857a02b6cd9
4632-38 <sadikaarjun55@gmail.com>
2026-06-22 11:40:27 +0545

    set up

---
+++ backend/src/controllers/requestController.js
@@ -0,0 +1,7 @@
+/**
+ * Blood Request Controller
+ * Handles blood request creation and management
+ */
+
+const BloodRequest = require('../models/BloodRequest');
+const Donor = require('../models/Donor');
